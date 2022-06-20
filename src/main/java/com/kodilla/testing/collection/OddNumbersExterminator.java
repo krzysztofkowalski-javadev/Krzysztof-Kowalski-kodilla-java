6a9804d9931e7b7fc1cf3a372d5673c858d1d3b2
Krzysztof Kowalski <krzysztofkowalski.javadev@gmail.com>
2022-06-20 23:35:01 +0200

    6.3 Added test forum and collection

---
+++ src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,28 @@
+package com.kodilla.testing.collection;
+
+import java.util.*;
+
+public class OddNumbersExterminator {
+
+    List<Integer> evenList;
+
+    public OddNumbersExterminator() {
+        evenList = new ArrayList<Integer>();
+    }
+
+    public List<Integer> exterminate(List<Integer> numbers) {
+        int temporaryValue = 0;
+
+        for (int n = 0; n < numbers.size(); n++) {
+            temporaryValue = numbers.get(n);
+            if (temporaryValue % 2 == 0) {
+                evenList.add(temporaryValue);
+            }
+        }
+            return evenList;
+        }
+        public List<Integer> getEvenList () {
+            return evenList;
+        }
+    }
+
